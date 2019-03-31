ab8c5c96c07340a4d707dd0600e7b0768b42cc7e
Salem Alshehri <sal5m@hotmail.com>
2019-03-31 10:50:00 +0300

    added post and delete req

--- src/routes/example_routes.js
+++ src/routes/example_routes.js
@@ -11,4 +11,42 @@ router.get("/example", tokenAuth, (req, res, next) => {
   // start a promise chain, so that any errors will pass to `handle`
 });
 
+router.get("/api/products", (req, res) => {
+  models.Product.findAll()
+    .then(products => {
+      res.status(200).json({ products: products });
+    })
+    .catch(e => console.log(e));
+});
+
+
+
+router.post("/api/products", (req, res) => {
+  console.log(req.body)
+  models.Product.create(req.body.products)
+  .then(product => {
+    res.status(200).json({
+      product: product
+    });
+  })
+  .catch(e => console.log(e))
+});
+
+
+
+router.delete('/api/products/:id', (req, res) => {
+  models.Product.findByPk(req.params.id)
+  .then(product => {
+     product.destroy()
+       .then(() => {
+       res.status(200).json({
+       result: `Record ID ${req.params.id} Deleted`,
+       success: true
+      })
+    })
+    .catch(e => console.log(e))
+   })
+   .catch(e => console.log(e))
+})
+
 export default router;
